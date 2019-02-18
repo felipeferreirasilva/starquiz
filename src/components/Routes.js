2a8ea5f3044a73d667d99a8fe93547d3ed40e435
Felipe Silva <felipeferreirasilva@gmail.com>
2019-02-18 13:32:02 -0300

    Initial commit

---
+++ src/components/Routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from './Home'
+
+const Routes = () => {
+    return (
+        <Route exact path="/" component={Home} />
+    )
+}
+
+export default Routes
---
+++ src/utils/googleImages.js
@@ -0,0 +1,2 @@
+export const CSE_ID = "018057695921300292746"
+export const API_KEY = "AIzaSyBu64CDCocqkR6k9DzZebo5z93D8QMp7cg"
