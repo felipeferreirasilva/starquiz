196ede928f324f9e743993f54b1e40f670091356
Felipe Silva <felipeferreirasilva@gmail.com>
2019-02-19 21:15:55 -0300

    Add comments

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,19 +5,18 @@ import Countdown from 'react-countdown-now';
 import { connect } from 'react-redux'
 import { gameStatus } from '../actions'
 
+// FUNÇAO É CHAMADA APÓS O TERMINO DO CONTADOR
+// SETA O STATUS DO JOGO PARA FALSE (TEMPO ACABOU)
 const Timer = props => {
-    // FUNÇAO É CHAMADA APÓS O TERMINO DO CONTADOR
-    // SETA O STATUS DO JOGO PARA FALSE (TEMPO ACABOU)
     const updateGameStatus = () => {
         props.dispatch(gameStatus(false))
     }
-
     return (
         <div>
             {/* ESCONDE / EXIBE O TIMER APOS CHECAR O STATUS DO JOGO */}
             {props.game.status &&
                 <div className="text-right mr-5 mt-2 animated slideInRight">
-                <h4 className="text-right">Tempo</h4>
+                    <h4 className="text-right">Tempo</h4>
                     <FontAwesomeIcon icon={faStopwatch} size="2x" />
                     <h3 className="float-right ml-2">
                         {/* TIMER QUE APOS 2 MINUTOS (120000) RETORNA UMA FUNÇAO */}
@@ -27,7 +26,6 @@ const Timer = props => {
             }
         </div>
     )
-
 }
 
 const mapStateToProps = state => {
