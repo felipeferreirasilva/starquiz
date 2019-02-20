9503a8011ae2366bd22cfa77e91eaa6928d08c4a
Felipe Silva <felipeferreirasilva@gmail.com>
2019-02-20 13:12:19 -0300

    Add IDs linkLogo logo linkHome linkAbout linkRanking

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { LOGO } from '../utils/images'
+
+// PARA UTLIZAR O HISTORY EM UM COMPONENT FORA DO <ROUTER />
 import { withRouter } from "react-router";
 
 export class Nav extends Component {
@@ -8,20 +10,20 @@ export class Nav extends Component {
         const pathName = this.props.location.pathname
         return (
             <nav className="navbar navbar-expand-md navbar-light bg-light mb-3 animated slideInDown fixed-top scrolling-navbar">
-                <Link className="navbar-brand" to="/"><img src={LOGO} alt="" className="img-fluid" style={style.logo}></img><span className="ml-2"><strong>StarQuiz</strong></span></Link>
+                <Link id="linkLogo" className="navbar-brand" to="/"><img id="logo" src={LOGO} alt="" className="img-fluid" style={style.logo}></img><span className="ml-2"><strong>StarQuiz</strong></span></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className={`nav-item ${pathName === '/' && 'active'}`}>
-                            <Link className="nav-link" to="/">Inicio</Link>
+                            <Link id="linkHome" className="nav-link" to="/">Inicio</Link>
                         </li>
                         <li className={`nav-item ${pathName === '/about' && 'active'}`}>
-                            <Link className="nav-link" to="/about">Sobre</Link>
+                            <Link id="linkAbout" className="nav-link" to="/about">Sobre</Link>
                         </li>
                         <li className={`nav-item ${pathName === '/ranking' && 'active'}`}>
-                            <Link className="nav-link" to="/ranking">Placar</Link>
+                            <Link id="linkRanking" className="nav-link" to="/ranking">Placar</Link>
                         </li>
                     </ul>
                 </div>
@@ -36,6 +38,7 @@ const style = {
     }
 }
 
+// CONECTA O COMPONENT COM O <ROUTER />
 const NavWithRouter = withRouter(Nav)
 
 export default (NavWithRouter)
