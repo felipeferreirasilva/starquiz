6383b07e7ad62728c833fb738d38ceee818c5b08
Felipe Silva <felipeferreirasilva@gmail.com>
2019-02-19 21:51:05 -0300

    Add Tests

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -3,24 +3,49 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
 
-// PARA PASSAR O HISTORY COMO PROP GLOBAL
+// CRIA MOCK DO HISTORY PARA UTILIZAR COMO PROP
 import { createMemoryHistory } from 'history'
 
 // COMPONENTS
 import { Game } from '../components/Game'
 import Spinner from '../components/Spinner'
+import Timer from '../components/Timer'
+import Card from '../components/Card'
+import GameOver from '../components/GameOver'
+
 
 describe('<Game />', () => {
     let wrapper;
     const history = createMemoryHistory('/')
+
     beforeEach(() => {
-        // PROPS QUE VAO NO LIFE CYCLE OU STORE
-        wrapper = shallow(<Game game={{status: true, score: 0}} history={history}/>)
+        // PROPS QUE VAO NO LIFE CYCLE / STORE
+        wrapper = shallow(<Game game={{ status: true, score: 0 }} history={history} />)
     })
-    it('CONTEM SPINNER QUANDO LOADING É IGUAL A TRUE', () => {
-        // DEFINE STATES ANTES DE BUSCAR CONTEUDO NO COMPONENT
-        wrapper.setState({loading: true, cards:{}})
-        let spinner = wrapper.find(Spinner)
-        expect(spinner.length).toBe(2)
-    });
+
+    it('RENDER SPINNER QUANDO state.loading=true', () => {
+        wrapper.setState({ loading: true })
+        expect(wrapper.find(Spinner).length).toBe(1)
+    })
+
+    it('RENDER TIMER QUANDO state.loading=false E props.game.status=true', () => {
+        wrapper.setState({ loading: false })
+        expect(wrapper.find(Timer).length).toBe(1)
+    })
+
+    it('RENDER CARD QUANDO state.loading=false E props.game.status=true E state.cards > 0', () => {
+        wrapper.setState({ loading: false, cards: [{ name: 'card' }] })
+        expect(wrapper.find(Card).length).toBe(1)
+    })
+
+    it('RENDER 2 BOTOES QUANDO state.loading=false E props.game.status=true E state.cards > 0', () => {
+        wrapper.setState({ loading: false, cards: [{ name: 'card' }] })
+        expect(wrapper.find('button').length).toBe(2)
+    })
+
+    it('RENDER GAMEOVER QUANDO state.loading=false E props.game.status=false', () => {
+        wrapper = shallow(<Game game={{ status: false, score: 0 }} history={history} />)
+        wrapper.setState({ loading: false })
+        expect(wrapper.find(GameOver).length).toBe(1)
+    })
 })
--- src/tests/Nav.test.js
+++ src/tests/Nav.test.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
 
 import Nav from '../components/Nav'
-
 import { Link } from 'react-router-dom'
-
-Enzyme.configure({ adapter: new Adapter() })
 
 describe('<Nav />', () => {
     let wrapper;
+
     beforeEach(() => {
         wrapper = shallow(<Nav />)
     })
-    it('Deve conter 3 links se nao estiver logado.', () => {
-        let links = wrapper.find(Link)
-        expect(links.length).toBe(3)
-    });
+
+    it('RENDER 3 LINKS, LOGO, HOME, RANKING', () => {
+        expect(wrapper.find(Link).length).toBe(3)
+    })
 })
