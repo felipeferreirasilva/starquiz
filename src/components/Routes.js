36764c8a74d2ae17fbe93388d7216c4ed9e6c364
Felipe Silva <felipeferreirasilva@gmail.com>
2019-02-20 10:36:33 -0300

    Add About Route

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Home from './Home'
+import About from './About'
 import Game from './Game'
 import Ranking from './Ranking'
 
@@ -8,6 +9,7 @@ const Routes = () => {
     return (
         <div>
             <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
             <Route path="/game" component={Game} />
             <Route path="/ranking" component={Ranking} />
         </div>
