d9f138c509e4cefa0f2778db4dd31b864a11f337
Felipe Silva <felipeferreirasilva@gmail.com>
2019-02-20 13:28:42 -0300

    Change Text Content

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,13 @@ const About = () => {
                 <h1><strong>Sobre o Jogo</strong></h1>
                 <p className="lead mt-3">StarQuiz é um jogo onde você deve testar os seus conhecimentos sobre os personagens da franquia Star Wars.</p>
                 <hr className="my-4" />
-                <h3 className="mt-2">Como Jogar?</h3>
+                <h3 className="mt-2"><strong>Como Jogar?</strong></h3>
                 <p>Ao iniciar o jogo, clique no botão advinhar e digite o nome do personagem na caixa, em seguida clique em OK para verificar se acertou ou errou.</p>
-                <h3 className="mt-2">Tempo</h3>
+                <h3 className="mt-2"><strong>Tempo</strong></h3>
                 <p>Quando o jogo começar, você tera dois minutos para tentar advinhar o máximo de personagens que conseguir.</p>
-                <h3 className="mt-2">Pontuação</h3>
-                <p>Cada personagem que você acertar vale 10 pontos, se você clicar no cartão de detalhes do personagem essa pontuação vai para 5 pontos.</p>
-                <h3 className="mt-2">Ranking</h3>
+                <h3 className="mt-2"><strong>Pontuação</strong></h3>
+                <p>Cada personagem que você acertar vale 10 pontos, se você clicar no cartão de detalhes do personagem essa pontuação vai para 5 pontos. O cartão do personagem mudará de cor quando você enviar o seu palpite, se ficar verde significa que você acertou, se ficar vermelho significa que você errou.</p>
+                <h3 className="mt-2"><strong>Ranking</strong></h3>
                 <p>Após o fim do jogo, você pode verificar a sua pontuação e decidir se quer deixar registrado em nosso ranking de jogadores, para isso, basta preencher o formulário com seu nome e email.</p>
             </div>
         </div>
